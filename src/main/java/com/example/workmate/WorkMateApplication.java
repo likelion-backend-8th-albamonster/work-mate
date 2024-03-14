ecad2e570cd10b6719bdfea3ce1860aba0910773
jisoo0609 <jisu20000609@gmail.com>
2024-03-14 09:55:16 +0900

    init

---
+++ src/main/java/com/example/workmate/WorkMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.workmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkMateApplication.class, args);
+	}
+
+}
