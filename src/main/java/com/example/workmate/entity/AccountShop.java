1e7e5ee4d925435e11d0f92fc6201c66b9bedd2e
ydh511 <ydh511@gmail.com>
2024-03-14 17:31:23 +0900

    for merging

--- src/main/java/com/example/workmate/entity/AccountShop.java
+++ src/main/java/com/example/workmate/entity/AccountShop.java
@@ -5,10 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-import java.util.List;
-
 @Entity
-public class Account {
+public class AccountShop {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/example/workmate/entity/Shop.java
+++ src/main/java/com/example/workmate/entity/Shop.java
@@ -2,15 +2,15 @@ package com.example.workmate.entity;
 
 import jakarta.persistence.*;
 
+
 @Entity
 public class Shop {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    @ManyToOne
-    @JoinColumn(name = "accountId")
-    private Account account;
     private String address;
+
+    @OneToMany(mappedBy = "shop")
+    private List<AccountShop> accountShops;
 }
