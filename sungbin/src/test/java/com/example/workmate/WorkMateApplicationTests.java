1f0ed132ff205640ce987e20bfeb9f87b116d448
binsjangjangman <tjdqls5198@gmail.com>
2024-03-14 11:21:31 +0900

    sungbin

---
+++ sungbin/src/test/java/com/example/workmate/WorkMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
