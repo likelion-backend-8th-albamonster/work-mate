3bef4a88aad5134cc09bcfce6ef785ac98640db3
“mafatofu” <qwqw0310@gmail.com>
2024-03-18 17:50:55 +0900

    Fix: 출근요청 기능 추가중
    출근 데이터가 없다면 출근 / 있다면 퇴근으로
    출근페이지에 버튼이 나타남
    버튼을 누르면 출퇴근 데이터 저장

--- src/main/java/com/example/workmate/service/AttendanceService.java
+++ src/main/java/com/example/workmate/service/AttendanceService.java
@@ -1,6 +1,7 @@
 package com.example.workmate.service;
 
-import com.example.workmate.dto.AttendanceDto;
+import com.example.workmate.dto.attendance.AttendanceDto;
+import com.example.workmate.dto.community.ArticleDto;
 import com.example.workmate.entity.Attendance;
 import com.example.workmate.entity.Shop;
 import com.example.workmate.entity.Status;
@@ -44,6 +45,70 @@ public class AttendanceService {
 
         //오늘 날짜에 이미 기록된 출근이 있는지 확인
         //db에서 사용자 id와 매장 id로 가장 최근 데이터 하나 꺼내오기
+        if (isExistTodayCheckIn(userId, shopId)){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "중복 출근 요청입니다.");
+        }
+
+        //저장
+        Attendance attendance = Attendance.builder()
+                .account(account)
+                .shop(shop)
+                .checkInTime(LocalDateTime.now())
+                .status(Status.IN)
+                .build();
+        return AttendanceDto.fromEntity(attendanceRepo.save(attendance));
+
+    }
+
+    //퇴근요청
+    //퇴근시간이 자신의 근무종료시간보다 늦다면 추가정산이 이루어진다.
+    @Transactional
+    public AttendanceDto checkOut(Long userId, Long shopId){
+        //사용자 확인
+        Account account = accountRepo.findById(userId)
+                .orElseThrow(
+                        ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자 정보를 확인해주세요")
+                );
+        //매장 확인
+        Shop shop = shopRepo.findById(shopId)
+                .orElseThrow(
+                        ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "매장 정보를 확인해주세요")
+                );
+
+        //오늘 날짜의 출근 데이터 status 수정
+        Attendance attendance = Attendance.builder()
+                .account(account)
+                .shop(shop)
+                .checkInTime(LocalDateTime.now())
+                .status(Status.OUT)
+                .build();
+        return AttendanceDto.fromEntity(attendanceRepo.save(attendance));
+    }
+    //쉬는시간요청
+    public void restIn(){
+
+    }
+    //쉬는시간종료요청
+    public void restOut(){
+
+    }
+
+    //오늘 날짜에 이미 기록된 출근이 있는지 확인
+    public boolean isExistTodayCheckIn(Long userId, Long shopId){
+        //사용자 확인
+        Account account = accountRepo.findById(userId)
+                .orElseThrow(
+                        ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자 정보를 확인해주세요")
+                );
+        //매장 확인
+        Shop shop = shopRepo.findById(shopId)
+                .orElseThrow(
+                        ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "매장 정보를 확인해주세요")
+                );
+        //체크용
+        boolean isExist = false;
+        //오늘 날짜에 이미 기록된 출근이 있는지 확인
+        //db에서 사용자 id와 매장 id로 가장 최근 데이터 하나 꺼내오기
         Optional<Attendance> attendanceCk
                 = attendanceRepo.findTopByAccountAndShopOrderByCheckInTimeDesc(account, shop);
         if (attendanceCk.isPresent()){
@@ -53,33 +118,11 @@ public class AttendanceService {
             LocalDate attendanceToday = attendanceCk.get().getCheckInTime().toLocalDate();
             //같은 날짜인지 비교
             if (today.isEqual(attendanceToday)){
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "중복 출근 요청입니다.");
+                //throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "중복 출근 요청입니다.");
+                isExist = true;
             }
         }
-
-        //저장
-        Attendance attendance = Attendance.builder()
-                .account(account)
-                .shop(shop)
-                .checkInTime(LocalDateTime.now())
-                //.checkInTime(LocalDateTime.of(2024,3,14,13,10,17))
-                .status(Status.OK)
-                .build();
-        return AttendanceDto.fromEntity(attendanceRepo.save(attendance));
-
-    }
-    //퇴근요청
-    //퇴근시간이 자신의 근무종료시간보다 늦다면 추가정산이 이루어진다.
-    public void checkOut(){
-
-    }
-    //쉬는시간요청
-    public void restIn(){
-
-    }
-    //쉬는시간종료요청
-    public void restOut(){
-
+        return isExist;
     }
 
     //출퇴근 기록 보기
@@ -108,6 +151,26 @@ public class AttendanceService {
     //Status를 수정하여, 정상출근 / 지각 / 조퇴 상태 변경
     public void udpate(){
 
+    }
+
+    //하나 찾기
+    public AttendanceDto readOne(Long userId, Long shopId){
+        //사용자 확인
+        Account account = accountRepo.findById(userId)
+                .orElseThrow(
+                        ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자 정보를 확인해주세요")
+                );
+        //매장 확인
+        Shop shop = shopRepo.findById(shopId)
+                .orElseThrow(
+                        ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "매장 정보를 확인해주세요")
+                );
+        Attendance attendance
+                = attendanceRepo.findTopByAccountAndShopOrderByCheckInTimeDesc(account, shop)
+                .orElseThrow(
+                        ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "출퇴근 정보를 확인해주세요")
+                );
+        return AttendanceDto.fromEntity(attendance);
     }
 
     //TODO 검색기능
