52ad10e33899766d55ed80a847e3ffa9ebd8ce5d
“mafatofu” <qwqw0310@gmail.com>
2024-03-26 16:18:36 +0900

    Fix: 출퇴근 기록확인페이지 fix
    path와 requestparam으로 인한 오류 fix /
    출퇴근 데이터를 가져올 때 권한 확인하여 관리자 전용 데이터를 가져오도록 fix

--- src/main/java/com/example/workmate/repo/attendance/AttendanceRepoDsl.java
+++ src/main/java/com/example/workmate/repo/attendance/AttendanceRepoDsl.java
@@ -3,9 +3,13 @@ package com.example.workmate.repo.attendance;
 import com.example.workmate.dto.attendance.AttendanceDto;
 import com.example.workmate.dto.attendance.AttendanceLogDto;
 import com.example.workmate.dto.attendance.AttendanceLogUpdateDto;
+import com.example.workmate.entity.AccountShop;
 import com.example.workmate.entity.QShop;
+import com.example.workmate.entity.account.Authority;
 import com.example.workmate.entity.attendance.QAttendance;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.CollectionExpression;
+import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -54,7 +58,7 @@ public class AttendanceRepoDsl {
     public Page<AttendanceLogDto> readUserAttendanceLog(Long accountId, Pageable pageable){
         QAttendance qAttendance = new QAttendance("attendance");
         QShop qShop = new QShop("shop");
-
+        
         // 쿼리의 결과를 DTO 클래스로 매핑해서 가져오기
         List<AttendanceLogDto> attendanceLogDtoList =
                 queryFactory.select(
@@ -83,8 +87,9 @@ public class AttendanceRepoDsl {
         );
     }
 
-    //한 사용자의 한 매장에 대한 정보
-    public Page<AttendanceLogDto> readUserOneShopAttendanceLog(Long accountId, Long shopId, Pageable pageable){
+    //여러 매장의 모든 출근 정보(관리자)
+    //shopId를 받아올 수 없는 상황이라, 메서드 분리
+    public Page<AttendanceLogDto> readUserAttendanceLogForAdmin(List<Long> accountShopList, Pageable pageable){
         QAttendance qAttendance = new QAttendance("attendance");
         QShop qShop = new QShop("shop");
 
@@ -103,14 +108,75 @@ public class AttendanceRepoDsl {
                         )
                         .from(qAttendance)
                         .innerJoin(qAttendance.shop, qShop)
-                        .where(
-                                qAttendance.account.id.eq(accountId),
-                                qAttendance.shop.id.eq(shopId)
-                        )
+                        .where(qAttendance.shop.id.in(accountShopList))
                         .offset(pageable.getOffset())//페이지번호
                         //.limit(pageable.getPageSize())//페이지사이즈
                         .fetch();
 
+
+        return new PageImpl<>(
+                attendanceLogDtoList,
+                pageable,
+                attendanceLogDtoList.size()
+        );
+    }
+
+
+    //한 사용자의 한 매장에 대한 정보
+    public Page<AttendanceLogDto> readUserOneShopAttendanceLog(Long accountId, Long shopId, Pageable pageable, Authority authority){
+        QAttendance qAttendance = new QAttendance("attendance");
+        QShop qShop = new QShop("shop");
+        List<AttendanceLogDto> attendanceLogDtoList
+                = new ArrayList<>();
+        //관리자
+        if (authority != Authority.ROLE_USER){
+            // 쿼리의 결과를 DTO 클래스로 매핑해서 가져오기
+            attendanceLogDtoList =
+                    queryFactory.select(
+                                    Projections.constructor(AttendanceLogDto.class,
+                                            qAttendance.id,
+                                            qAttendance.account.id,
+                                            qAttendance.shop.id,
+                                            qAttendance.checkInTime,
+                                            qAttendance.checkOutTime,
+                                            qAttendance.status,
+                                            qShop.name
+                                    )
+                            )
+                            .from(qAttendance)
+                            .innerJoin(qAttendance.shop, qShop)
+                            .where(
+                                    qAttendance.shop.id.eq(shopId)
+                            )
+                            .offset(pageable.getOffset())//페이지번호
+                            //.limit(pageable.getPageSize())//페이지사이즈
+                            .fetch();
+        } 
+        //일반사용자
+        else {
+            // 쿼리의 결과를 DTO 클래스로 매핑해서 가져오기
+            attendanceLogDtoList =
+                    queryFactory.select(
+                                    Projections.constructor(AttendanceLogDto.class,
+                                            qAttendance.id,
+                                            qAttendance.account.id,
+                                            qAttendance.shop.id,
+                                            qAttendance.checkInTime,
+                                            qAttendance.checkOutTime,
+                                            qAttendance.status,
+                                            qShop.name
+                                    )
+                            )
+                            .from(qAttendance)
+                            .innerJoin(qAttendance.shop, qShop)
+                            .where(
+                                    qAttendance.account.id.eq(accountId),
+                                    qAttendance.shop.id.eq(shopId)
+                            )
+                            .offset(pageable.getOffset())//페이지번호
+                            //.limit(pageable.getPageSize())//페이지사이즈
+                            .fetch();
+        }
 
         return new PageImpl<>(
                 attendanceLogDtoList,
@@ -123,7 +189,7 @@ public class AttendanceRepoDsl {
     public Page<AttendanceLogDto> readOneShopAttendanceLog(Long shopId, Pageable pageable){
         QAttendance qAttendance = new QAttendance("attendance");
         QShop qShop = new QShop("shop");
-
+        List<Long> asdf = new ArrayList<>();
         // 쿼리의 결과를 DTO 클래스로 매핑해서 가져오기
         List<AttendanceLogDto> attendanceLogDtoList =
                 queryFactory.select(
