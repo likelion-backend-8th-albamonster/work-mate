0fb1a1ddbbda74d9ff5ad28318b188b813ab2140
ydh511 <ydh511@gmail.com>
2024-04-02 17:42:26 +0900

    feat : url
    body : WebSecurityConfig에 url 추가완료
    
    tail : 화이팅

--- src/main/java/com/example/workmate/controller/schedule/ScheduleController.java
+++ src/main/java/com/example/workmate/controller/schedule/ScheduleController.java
@@ -5,6 +5,10 @@ import com.example.workmate.dto.schedule.PeriodScheduleDto;
 import com.example.workmate.dto.schedule.WorkTimeDto;
 import com.example.workmate.dto.schedule.ChangeRequestDto;
 import com.example.workmate.dto.schedule.ScheduleDto;
+import com.example.workmate.dto.shop.ShopDto;
+import com.example.workmate.entity.Shop;
+import com.example.workmate.entity.schedule.QChangeRequest;
+import com.example.workmate.service.ShopService;
 import com.example.workmate.service.schedule.ScheduleDataService;
 import com.example.workmate.service.schedule.ScheduleService;
 import lombok.RequiredArgsConstructor;
@@ -31,33 +35,8 @@ import java.util.List;
 @CrossOrigin
 public class ScheduleController {
     private final ScheduleService scheduleService;
-    private final ScheduleDataService scheduleDataService;
     private final ScheduleUtil scheduleUtil;
-
-    // 사람, 상점, accountShop 만들기
-    @ResponseBody
-    @PostMapping("/account-shop")
-    public String accountshop(){
-        scheduleDataService.accountShop();
-        return "done";
-    }
-
-    // 한 사람의 한 달 동안의 랜덤시간 근무 만들기
-    @ResponseBody
-    @PostMapping("/make")
-    public String make(
-            @RequestParam
-            Long accountId,
-            @RequestParam
-            Long shopId,
-            @RequestParam
-            int year,
-            @RequestParam
-            int month
-    ){
-        scheduleDataService.makeWorkTime(accountId,shopId,year,month);
-        return "done";
-    }
+    private final ShopService shopService;
 
     // 근무표 메인페이지
     @RequestMapping("/{shopId}")
@@ -81,15 +60,23 @@ public class ScheduleController {
             log.info("auth is null");
         PeriodScheduleDto periodScheduleDto = new PeriodScheduleDto();
         List<WorkTimeDto> schedules = scheduleService.viewMonth(shopId, dto);
+        ShopDto shopDto = shopService.readOneShop(shopId);
+        if (auth != null){
+            model.addAttribute("authority",auth.getAuthorities().toString());
+            model.addAttribute("username",auth.getName());
+        }else{
+            model.addAttribute("authority", "anonymous");
+            model.addAttribute("username",null);
+        }
         model.addAttribute("periodScheduleDto", periodScheduleDto);
         model.addAttribute("schedules", schedules);
-        model.addAttribute("shopId", shopId);
+        model.addAttribute("shop", shopDto);
         model.addAttribute("calender",scheduleUtil.makeCalender(dto));
         return "schedule/monthly-schedule";
     }
 
     // 기간 동안의 근무표리스트 보기
-    @PostMapping("/find-schedule/{shopId}")
+    @PostMapping("/list-schedule/{shopId}")
     public String findSchedule(
             Model model,
             @PathVariable("shopId")
@@ -108,127 +95,38 @@ public class ScheduleController {
         model.addAttribute("listSchedules",listSchedules);
         return "schedule/list-schedule";
     }
-    //ㅇㅅㅇ
-    @PostMapping("/list-schedule/{shopId}")
-    public List<LocalDate> listSchedule(
-            @PathVariable("shopId")
-            Long shopId,
-            PeriodScheduleDto dto
-    ){
-        return scheduleUtil.listSchedule(dto.getStartDate(),dto.getEndDate());
-    }
 
-    // 근무 만들기
-    @ResponseBody
-    @PostMapping("/create")
-    public WorkTimeDto create(
-            @RequestParam WorkTimeDto dto
-    ){
-        return scheduleService.create(dto);
-    }
-
-    // 근무 수정하기
-    @ResponseBody
-    @GetMapping("/update/{workTimeId}")
-    public WorkTimeDto update(
-            @RequestParam WorkTimeDto dto,
-            @PathVariable("workTimeId")
-            Long workTimeId
-            // git check
-    ){
-        return scheduleService.update(workTimeId, dto);
-    }
-
-    // 근무 모두 보기
-    @ResponseBody
-    @GetMapping("/read/{shopId}")
-    public Page<WorkTimeDto> readPage(
-            Pageable pageable,
+    // 근무 교체할 시간 고르기
+    @GetMapping("/change-worktime/{shopId}")
+    public String changeSchedule(
+            Model model,
             @PathVariable("shopId")
             Long shopId
     ){
-        return scheduleService.readPage(shopId, pageable);
+        ChangeRequestDto dto = new ChangeRequestDto();
+        LocalDate date = LocalDate.now();
+        // 현재의 시간보다 미래의 시간만 교체신청 할 수 있다.
+        model.addAttribute("shopId",shopId);
+        model.addAttribute("changeRequestDto", dto);
+
+        return "schedule/change-worktime";
     }
 
-    //근무 하나 보기
-    @ResponseBody
-    @GetMapping("/read-one/{workTimeId}")
-    public WorkTimeDto readOne(
-            @PathVariable("workTimeId")
-            Long workTimeId
-    ){
-        return scheduleService.readOne(workTimeId);
-    }
-    // 근무 지우기
-    @ResponseBody
-    @DeleteMapping("/delete/{workTimeId}")
-    public WorkTimeDto delete(
-            @PathVariable("workTimeId")
-            Long workTimeId
-    ){
-        return scheduleService.delete(workTimeId);
-    }
-
-    // 한달 씩으로 보기.
-    @ResponseBody
-    @GetMapping("view-month/{shopId}")
-    public List<WorkTimeDto> viewMonth(
-            @PathVariable("shopId")
-            Long shopId,
-            @RequestBody
-            ScheduleDto month
-    ){
-        return scheduleService.viewMonth(shopId, month);
-    }
-
-    // 시작 기간과 끝 기간을 정해서 보기 0번은 시작, 1번은 끝
-    @ResponseBody
-    @GetMapping("view-period/{shopId}")
-    public List<WorkTimeDto> viewPeriod(
-            @PathVariable("shopId")
-            Long shopId,
-            @RequestBody
-            PeriodScheduleDto dto
-    ){
-        return scheduleService.viewPeriod(shopId,dto.getStartDate(), dto.getEndDate());
-    }
-
-    // 근무표 변경요청만들기
-    @ResponseBody
-    @PostMapping("create-change")
-    public ChangeRequestDto changeRequest(
-            @RequestBody
-            ChangeRequestDto dto
-    ){
-        return scheduleService.createChange(dto);
-    }
-    // 근무표 변경요청 보기, 아직 제안 중인 것만
-    @ResponseBody
-    @GetMapping("read-change-all/{shopId}")
-    public List<ChangeRequestDto> readChangeAll(
+    // 근무표 관리
+    @RequestMapping("/manage-schedule/{shopId}")
+    public String manageSchedule(
             @PathVariable("shopId")
             Long shopId
     ){
-        return scheduleService.readChangeAll(shopId);
+        return "schedule/manage-schedule";
     }
 
-    // 근무 교체 승인
-    @ResponseBody
-    @PutMapping ("confirm-change")
-    public ChangeRequestDto confirmChange(
-            @RequestParam("changeRequestId")
-            Long changeRequestId
+    // 근무교체 보기
+    @RequestMapping("/view-change-worktime/{shopId}")
+    public String viewChangeWorkTime(
+            @PathVariable("shopId")
+            Long shopId
     ){
-        return scheduleService.confirmChange(changeRequestId);
-    }
-
-    //근무 교체 거절
-    @ResponseBody
-    @PutMapping("decline-change")
-    public ChangeRequestDto declineChange(
-            @RequestParam("changeRequestId")
-            Long changeRequestId
-    ){
-        return scheduleService.declineChange(changeRequestId);
+        return "schedule/view-change-worktime";
     }
 }
